5e7e333cc35dc26f18ea7753dcaed2aa87d5bcde
Nik Pruss <nikpruss@gmail.com>
2021-05-13 20:46:08 -0500

    Part 1

---
+++ java-practice/Restaurant Menu/src/restaurant/Menu.java
@@ -0,0 +1,10 @@
+package restaurant;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Menu {
+    private ArrayList<MenuItem> menuItems;
+    private Date updated;
+
+}
---
+++ java-practice/Restaurant Menu/src/restaurant/MenuItem.java
@@ -0,0 +1,8 @@
+package restaurant;
+
+public class MenuItem {
+    private int price;
+    private String description;
+    private String category;
+    private boolean newItem;
+}
